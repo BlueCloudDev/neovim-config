872dfb72048ec7bae3e542f9ece6cdbbf7667a6b
JB <justin.blau8@gmail.com>
2025-05-27 13:12:32 -0400

    updates

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -20,7 +20,7 @@ return {
         },
         mapping = cmp.mapping.preset.insert({
           --['<C-Space>'] = cmp.mapping.complete(),
-          ['<CR>'] = cmp.mapping.confirm({ select = false }),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
           ['<Tab>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 's' }),
           ['<S-Tab>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 's' }),
         }),
